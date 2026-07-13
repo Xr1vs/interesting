1b6ee39488c355a4d503f59aacc2900e9dfc396a
Xr1vs <143473810+Xr1vs@users.noreply.github.com>
2026-07-13 16:58:38 -0600

    Delete api/roblox directory

--- api/roblox/keystatus.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    hasServerKey: Boolean(process.env.ROBLOX_API_KEY),
-    creatorType: process.env.ROBLOX_CREATOR_TYPE || null,
-    hasCreatorId: Boolean(process.env.ROBLOX_CREATOR_ID),
-  });
-};
